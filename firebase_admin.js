9d3512467eb78f55e4ba4c82d39f147f039c7c89
Md Sakibul Alam <js.team@brainstation-23.com>
2022-12-13 18:18:23 +0600

    remove console log

--- firebase_admin.js
+++ firebase_admin.js
@@ -1,8 +1,5 @@
 const { cert } = require("firebase-admin/app");
 const app = require("firebase-admin");
-// const serviceAccount = require("./config/serviceAccountKey.json");
-
-console.log(process.env.serviceAccount);
 
 app.initializeApp({ credential: cert(JSON.parse(process.env.serviceAccount)) });
 module.exports = app;
